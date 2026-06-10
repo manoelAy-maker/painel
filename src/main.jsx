c2314728e7ed04f0d8c2dc112149c15fc7c5c24f
manoelAy-maker <manoel.Ay@icloud.com>
2026-06-09 22:30:45 -0300

    Clean duplicate CSS imports from main

--- src/main.jsx
+++ src/main.jsx
@@ -3,15 +3,6 @@ import ReactDOM from 'react-dom/client'
 import { AppProvider } from './context/AppContext'
 import App from './App'
 import './index.css'
-import './portal-background.css'
-import './mobile-polish.css'
-import './mobile-captacao.css'
-import './login-mobile.css'
-import './clickup-workspace.css'
-import './captacao-pontuacao.css'
-import './captacao-leads-pro.css'
-import './captacao-simple-mode.css'
-import './captacao-desktop-pro.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
