fe1863afe41b666a31a6f4344617d4d6bb0668dd
manoelAy-maker <manoel.Ay@icloud.com>
2026-06-09 22:33:36 -0300

    Add captacao service migration point

---
+++ src/modules/captacao/services/captacaoService.js
@@ -0,0 +1,5 @@
+// AYRES v1 modularização gradual.
+// A lógica nova de captação usa supabaseV2.
+// Este arquivo vira a porta única do módulo Captação para banco/serviços.
+
+export * from '../../../lib/supabaseV2'
