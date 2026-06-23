d3d447ebe743ccaa1689c52187cdd0030db21b40
hakerdebh-commits <hakerdebh@gmail.com>
2026-06-23 18:31:25 -0300

    Importa tema AYRES premium

--- src/main.jsx
+++ src/main.jsx
@@ -5,6 +5,7 @@ import App from './App'
 import './index.css'
 import './styles/ayres-global-polish.css'
 import './styles/ayres-design-system.css'
+import './styles/ayres-tema.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
