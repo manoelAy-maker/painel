45c30569ef293c791c48f7f5dcceee0da6ca70ab
hakerdebh-commits <hakerdebh@gmail.com>
2026-06-22 22:17:55 -0300

    Importa layout limpo do formulario de estadia

--- src/App.jsx
+++ src/App.jsx
@@ -12,6 +12,7 @@ import './styles/app.css'
 import './cargo-options-runtime.js'
 import './login-dark-restore.css'
 import './styles/ayres-design-system.css'
+import './styles/ayres-estadia-form-clean.css'
 
 const EstadiaLancada = lazy(() => import('./modules/estadia/pages/EstadiaLancada'))
 const ConsultaEstadiasLancadas = lazy(() => import('./pages/ConsultaEstadiasLancadas'))
