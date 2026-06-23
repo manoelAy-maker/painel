bb8f481a89d305b627ac3d3e0d2d3064e78c8d11
hakerdebh-commits <hakerdebh@gmail.com>
2026-06-22 21:15:11 -0300

    Importa design system Ayres

--- src/main.jsx
+++ src/main.jsx
@@ -4,6 +4,7 @@ import { AppProvider } from './context/AppContext'
 import App from './App'
 import './index.css'
 import './styles/ayres-global-polish.css'
+import './styles/ayres-design-system.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
